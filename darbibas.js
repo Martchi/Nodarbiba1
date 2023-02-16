6b03d6bac55f3fe5fb8edfa50a97d61d19f4bf3b
Martchi <kawaiiheart84@gmail.com>
2023-02-16 08:59:52 +0200

    Izlabotas kludas

--- darbibas.js
+++ darbibas.js
@@ -1,2 +1,2 @@
-document.body.innerHTML=”<h1>Esmu jau ceļā uz izpratni par Github </h1>”;
-console.log(“Laimīga”);
+document.body.innerHTML="<h1>Esmu jau ceļā uz izpratni par Github </h1>";
+console.log("Laimīga");
